1cd950de6a22d6206d2f177e8c026404c26e870d
kumari soumya <f20221184@pilani.bits-pilani.ac.in>
2025-03-12 14:49:33 +0530

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,14 +1,19 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "lexerDef.h"
 #include <stdbool.h>
 #include <stdio.h>
-
-// Includes parserDef.h, which has token definitions and data structures
-#include "parserDef.h"
-TreeNode* parseInputSourceCode(FILE* testFile);
-
-
-// Error reporting function (prototype)
+extern FILE *srcFile;
+extern char *forward;
+extern char *lexemebegin;
+extern int activeBuffer;
+extern bool exhaustedInput;
+extern bool ldfirstBuff;
+extern bool ldsecondBuff;
+extern TwinBuffer twinBuffer; // Global TwinBuffer instance
+extern keyword *kwEntries[KC];
+extern Symboltable *table;
+/*-----symboltable------*/
 void reportSyntaxError(int line, const char *message);
 #endif
