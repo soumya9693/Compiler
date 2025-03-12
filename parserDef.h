b0f32b0960a763b32f7aa4cdcaf94f6f928256e4
kumari soumya <f20221184@pilani.bits-pilani.ac.in>
2025-03-13 00:51:58 +0530

    Update parserDef.h

--- parserDef.h
+++ parserDef.h
@@ -1,10 +1,3 @@
-//                         GROUP - 30
-// ID:  2020A7PS0094P                     Name:  Arya Veer Singh Chauhan
-// ID:  2020A7PS0049P                     Name:  Madhav Madhusoodanan
-// ID:  2020A7PS0016P                     Name:  Ruchika Sarkar
-// ID:  2020A7PS0984P                     Name:  Utsav Goel
-// ID:  2020A7PS0102P                     Name:  Hardik Jain
-
 #include<stdio.h>
 #ifndef PARSERDEF_H
 #define PARSERDEF_H
