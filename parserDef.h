633a46145a5e9aeb14fae277e49e593fdd0e4eda
kumari soumya <f20221184@pilani.bits-pilani.ac.in>
2025-03-12 14:35:46 +0530

    Update parserDef.h

--- parserDef.h
+++ parserDef.h
@@ -1,62 +1,66 @@
 #ifndef PARSERDEF_H
 #define PARSERDEF_H
 
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-#define EPSILON_ID 0
-#define END_MARKER_ID -1
-#define INITIAL_SET_SIZE 10
+// Token definitions 
+typedef enum {
+    TK_GT, TK_LT, TK_ASSIGNOP, TK_COMMENT, TK_EQ, TK_PLUS, TK_NE, TK_LE, TK_GE, 
+    TK_SQR, TK_SQL, TK_OR, TK_NOT, TK_AND, TK_ID, TK_FIELDID, TK_DIV, TK_MUL, 
+    TK_MINUS, TK_FUNID, TK_DOT, TK_CL, TK_OP, TK_COLON, TK_SEM, TK_RUID, 
+    TK_COMMA, TK_NUM, TK_RNUM, TK_MAIN, TK_AS, TK_CALL, TK_DEFINETYPE, 
+    TK_ELSE, TK_END, TK_ENDIF, TK_ENDWHILE, TK_ENDRECORD, TK_ENDUNION, 
+    TK_GLOBAL, TK_IF, TK_INPUT, TK_OUTPUT, TK_INT, TK_REAL, TK_LIST, 
+    TK_PARAMETERS, TK_PARAMETER, TK_READ, TK_WRITE, TK_RECORD, TK_UNION, 
+    TK_RETURN, TK_THEN, TK_TYPE, TK_WHILE, TK_WITH, EPS, END_OF_INPUT
+} terminals;
 
-// Symbol structure definition
+// Structure to hold token information
 typedef struct {
-    int id;
-    int isTerminal;
-    int isEpsilon;
-} Symbol;
+    terminals token;
+    int lineNo;
+    union {
+        char* str;    
+        int intVal;  
+        float floatVal;
+    } attribute;
+} tokenInfo;
 
-// Production Rule structure
-typedef struct {
-    Symbol lhs;
-    Symbol* rhs;
-    int numRhs;
-} Rule;
+// Symbol structure 
+typedef struct SymbolTableitem {
+    char *lexeme;
+    terminals token;
+    char *type;
+    union {
+        int intVal;
+        float floatVal;
+    } value;
+    int lineCount;
+    int scopeLevel;
+    int startPos;
+    int endPos;
+    char **parameter;
+    char *returnType;
+    struct Field *fields;
+    struct SymbolTableitem *next;
+} SymbolTableitem;
 
-// Grammar structure
-typedef struct {
-    Symbol startSymbol;
-    Symbol* nonTerminals;
-    int numNonTerminals;
-    Rule* rules;
-    int numRules;
-} Grammar;
+// Forward declaration of struct Field 
+typedef struct Field Field;
 
-// Set implementation for FIRST/FOLLOW
-typedef struct {
-    Symbol* symbols;
-    int count;
-    int capacity;
-} Set;
+// Grammar-related structures 
+typedef struct TreeNode {
+    char* symbol;          
+    tokenInfo token;        
+    struct TreeNode* children[10];  
+    int numChildren;
+} TreeNode;
 
-// Combined FIRST and FOLLOW sets
-typedef struct {
-    Set* first;
-    Set* follow;
-} FirstAndFollow;
+// Function prototypes (to be implemented in parser.c)
+TreeNode* parseInputSourceCode(FILE* testFile); // Main parsing function
 
-// Public interface
-FirstAndFollow ComputeFirstAndFollowSets(Grammar G);
+// Error reporting function (prototype)
+void reportSyntaxError(int line, const char *message);
 
-// Helper functions
-void computeFirstSets(Grammar G, FirstAndFollow* FF);
-void computeFollowSets(Grammar G, FirstAndFollow* FF);
-Set createEmptySet();
-int addToSet(Set* set, Symbol symbol);
-int unionSets(Set* dest, Set src);
-int containsEpsilon(Set set);
-int getNonTerminalIndex(Grammar G, Symbol nonTerminal);
-int symbolsEqual(Symbol s1, Symbol s2);
-Symbol createEpsilonSymbol();
-Symbol createEndMarkerSymbol();
-
-#endif
+#endif /* PARSERDEF_H */
