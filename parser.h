6d5c11d2aac104bc7e1927b5c2f95e7e64b5dbd4
kumari soumya <f20221184@pilani.bits-pilani.ac.in>
2025-03-13 15:43:30 +0530

    Update parser.h

--- parser.h
+++ parser.h
@@ -2,14 +2,16 @@
 #define PARSER_h
 #include "parserDef.h"
 
-TREENODE parser(char* grammarFile,char* inputFile,int size);
-void testAutomation(char* grammarFile);
-TREENODE getChildNonTerminal(int nt,TREENODE tn);
-TREENODE getChildTerminal(int nt,TREENODE tn);
-TREENODE* getDualNonTerminal(int nt,TREENODE tn);
-TREENODE* getDualTerminal(int nt,TREENODE tn);
-void inorderTraversal(TREENODE root,short goNext);
-int countParseTreeNodes(TREENODE tn, short goNext);
-int getSizePT();
-void setSizePT();
-#endif
+// Function to compute FIRST and FOLLOW sets
+void ComputeFirstAndFollowSets(Grammar G, FirstAndFollow *F);
+
+// Function to create a parse table
+void createParseTable(FirstAndFollow F, ParseTable *T);
+
+// Function to parse input source code
+ParseTree *parseInputSourceCode(char *testcaseFile, ParseTable T);
+
+// Function to print the parse tree
+void printParseTree(ParseTree *PT, char *outfile);
+
+#endif // PARSE_H
